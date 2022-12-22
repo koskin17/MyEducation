13f2976d3d78707f6a5becf517c5ea42abf0dcd4
koskin <118272123+koskin17@users.noreply.github.com>
2022-12-22 13:26:31 +0200

    Delete Обучение/CodeWars directory

--- Обучение/CodeWars/open_file_pull_ups.py
+++
@@ -1,9 +0,0 @@
-while True:
-    file = open("pull_ups.txt", "r+")
-    n = int(input())
-
-    listf = file.readlines()
-    print(listf[n-1])
-
-    file.close()
-
--- Обучение/CodeWars/определение века по году.py
+++
@@ -1,9 +0,0 @@
-''' Нужно определить век по полученному году'''
-year = int(input('Введите год: '))
-
-def century(year):
-    if year %100 == 0:
-        return int(year / 100)
-    return int(year/100) + 1
-
-print(century(year))
