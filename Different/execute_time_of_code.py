ebcac138a470347c5e505656d3cd6da5e65dc7d7
koskin17 <kostia.zeykin@gmail.com>
2022-12-27 08:51:03 +0200

    try time measurement of execute code

---
+++ Different/execute_time_of_code.py
@@ -0,0 +1,20 @@
+import datetime
+
+# Метод 1
+start = datetime.datetime.now()
+"""Some code"""
+print(datetime.datetime.now() - start)
+
+# Метод 2
+import time
+
+start_time = time.time()
+"""Some code"""
+print(f"Total time to execute code is {(time.time() - start_time)}")
+
+# Метод 3
+# import timeit
+#
+#
+# code = [2,3,6,5,8,9,15,14,18,19,54,36,98].sort()
+# print(timeit.timeit(stmy=code, number=1000))
