ad189be666530c3e589a8b8fc4df82b3275ab308
koskin17 <kostia.zeykin@gmail.com>
2023-01-08 09:55:48 +0200

    else in try/except

---
+++ Different/else in try except.py
@@ -0,0 +1,17 @@
+# В блоках try / except можно использовать ветку else.
+# Она будет выполняться, если не было возникло исключение
+print("Вариант 1:")
+try:
+    2 / 'a'
+except TypeError:
+    print("An exception was raised")
+else:
+    print("Good! No exception was raised!")
+
+print("Вариант 2:")
+try:
+    2 * 3
+except TypeError:
+    print("An exception was raised")
+else:
+    print("Good! No exception was raised!")
