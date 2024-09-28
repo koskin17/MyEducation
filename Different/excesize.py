e748ec9654a907f60ae3585adc95a7df7deb3b82
koskin <118272123+koskin17@users.noreply.github.com>
2024-09-28 13:32:22 +0300

    add exercise 2

--- Different/excesize.py
+++ Different/excesize.py
@@ -10,6 +10,37 @@
 #     array.append(int(a == val))
 # print(sum(array))
 
-a = 1; b = 2
-print(eval('print(a + b, end=" ") or 0'))
+# a = 1; b = 2
+# print(eval('print(a + b, end=" ") or 0'))
 
+# class A:
+#     def __iter__(self):
+#         yield from range(10)
+# a = A()
+# print(5 in a, 20 in a)
+
+# import re
+# sent = 'Hello, Ben! How are you doing?'
+# """split разбивает на 4 части строку.
+#     Filter, если первый параметр None, просто оставит те элементы, которые возвращают true.
+#     Пустая строка возвращает False"""
+# lister = filter(None, re.split("[,!?]+", sent))
+# print(re.split("[,!?]+", sent))
+# print(len(list(lister)))
+
+from itertools import product
+a = [1, 2]
+b = [3, 4]
+"""Product создаёт кортежи всевозможных пар, беря по очереди:
+    первый из а и первый из b,
+    потом первый из а, второй из b.
+    Потом второй из а, первый из b и т.д.
+    
+    При этом создаётся объект в памяти.
+    Для его получения нужно его преобразовать в список, кортеж и т.д.
+    """
+print(f"Список кортежей: {list(product(a, b))}")
+print(f"Кортеж кортежей: {tuple(product(a, b))}")
+print(f"Множество кортежей: {set(product(a, b))}")
+cort = tuple(product(a, b))
+print(sum(sum(cort, (0,))))
--- Different/execute_time_of_code.py
+++ Different/execute_time_of_code.py
@@ -1,18 +1,19 @@
 import datetime
 
-# Метод 1
-start = datetime.datetime.now()
-"""Some code"""
-print(datetime.datetime.now() - start)
+# # Метод 1
+# start = datetime.datetime.now()
+# """Some code"""
+# print(datetime.datetime.now() - start)
 
-# Метод 2
-import time
+# # Метод 2
+# import time
 
-start_time = time.time()
-"""Some code"""
-print(f"Total time to execute code is {(time.time() - start_time)}")
+# start_time = time.time()
+# """Some code"""
+# print(f"Total time to execute code is {(time.time() - start_time)}")
 
 # Метод 3
 import timeit
-code = [2,3,6,5,8,9,15,14,18,19,54,36,98].sort()
-print(timeit.timeit(stmy=code, number=1000))
+# code = [2,3,6,5,8,9,15,14,18,19,54,36,98].sort()
+# print(timeit.timeit(stmt=code, number=1000))
+print(timeit.timeit('char in text', setup='text = "sample string"; char = "g"'))
