437674cc2652265c849db8d3be3da1513394b845
koskin <118272123+koskin17@users.noreply.github.com>
2024-09-28 12:29:50 +0300

    add exercise 1

--- Different/excesize.py
+++ Different/excesize.py
@@ -1,11 +1,15 @@
-a = 0
-def func():
-    global a
-    for num in [2, 4, 8]:
-        a += 1
-        yield num * 0.5
+# a = 0
+# def func():
+#     global a
+#     for num in [2, 4, 8]:
+#         a += 1
+#         yield num * 0.5
         
-array = []
-for val in func():
-    array.append(int(a == val))
-print(sum(array))
+# array = []
+# for val in func():
+#     array.append(int(a == val))
+# print(sum(array))
+
+a = 1; b = 2
+print(eval('print(a + b, end=" ") or 0'))
+
