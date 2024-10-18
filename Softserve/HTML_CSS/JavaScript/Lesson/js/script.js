c29a2b8bf5903d358dd2105621e09f8766956c06
koskin <118272123+koskin17@users.noreply.github.com>
2024-10-18 14:14:47 +0300

    start studying javascript

---
+++ Softserve/HTML_CSS/JavaScript/Lesson/js/script.js
@@ -0,0 +1,11 @@
+const message="Привіт! Я JavaScript";
+alert(message);
+
+const additionalVariable="Привіт! Я JavaScript";
+alert(additionalVariable);
+
+let message2="Привіт! Я змінна";
+alert(message2);
+
+message2="Привіт! Я нове значення змінной";
+alert(message2);
