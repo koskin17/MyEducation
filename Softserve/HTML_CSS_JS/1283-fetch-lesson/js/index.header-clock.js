ab0bbb2e4a6254537ded01580d8423ef80e2d209
koskin <kostia.zeykin@gmail.com>
2024-12-30 19:07:27 +0200

    epty

--- Softserve/HTML_CSS_JS/1283-fetch-lesson/js/index.header-clock.js
+++
@@ -1,7 +0,0 @@
-const clockContainer = document.querySelector(".header__clock");
-
-function updateClock() {
-    clockContainer.innerText = new Date().toLocaleTimeString('uk');
-}
-
-setInterval(updateClock, 1000);
