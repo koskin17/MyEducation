8879e84edb098fa8caf8a9724e11f321161ef92e
koskin <kostia.zeykin@gmail.com>
2025-05-14 12:12:06 +0300

    little change code in task 5

--- Softserve/UA-130 Advanced Python with Django/Sprint 05/Task 5.py
+++ Softserve/UA-130 Advanced Python with Django/Sprint 05/Task 5.py
@@ -32,10 +32,14 @@ def check_number_group(number):
         return "You entered incorrect data. Please try again."
 
 # # Примеры вызова функции
-# check_number_group(4)       # Output: "We obtain error: Number of your group can't be less than 10"
-# check_number_group(59)      # Output: "Number of your group 59 is valid"
-# check_number_group("25")    # Output: "Number of your group 25 is valid"
-# check_number_group("abc")   # Output: "You entered incorrect data. Please try again."
+check_number_group(4)       # Output: "We obtain error: Number of your group can't be less than 10"
+check_number_group(59)      # Output: "Number of your group 59 is valid"
+check_number_group("25")    # Output: "Number of your group 25 is valid"
+check_number_group("abc")   # Output: "You entered incorrect data. Please try again."
+print(check_number_group(4))       # Output: "We obtain error: Number of your group can't be less than 10"
+print(check_number_group(59))      # Output: "Number of your group 59 is valid"
+print(check_number_group("25"))    # Output: "Number of your group 25 is valid"
+print(check_number_group("abc"))   # Output: "You entered incorrect data. Please try again."
 
 # Объяснение кода
 # - Создаём пользовательский класс исключения ToSmallNumberGroupError, который наследуется от Exception.
