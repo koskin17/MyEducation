8260c8dd36a3cc3c6df99546f1ec3148298de744
koskin <kostia.zeykin@gmail>
2025-05-21 15:01:26 +0300

    task 2 init

---
+++ Softserve/UA-130 Advanced Python with Django/Sprint 07 Design Patterns in Python/Task 2.py
@@ -0,0 +1,18 @@
+# Your task is to create an application for the departmental store. Initially, there was one and only one type of discount called the On-Sale-Discount (50%). But as time passes, the owner of the departmental store demands for including some other types of discount also for the customers. 
+
+# Please, solve the above-described problem in an efficient way. Our actual class should store the reference to one of the strategy function.
+
+# You have the structure of your future application in the answer box preload.
+
+# Заготовка кода:
+# class Goods: 
+  
+#     def __init__(self, price, discount_strategy = None): 
+          
+#     def price_after_discount(self): 
+          
+# def on_sale_discount(order): 
+      
+# def twenty_percent_discount(order): 
+      
+    
