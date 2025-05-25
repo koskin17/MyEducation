f2abf9f22d918379e0478d2f528fe94c2d7c7aaf
koskin <kostia.zeykin@gmail>
2025-05-25 11:40:42 +0300

    init task 3 from sprint 8

---
+++ Softserve/UA-130 Advanced Python with Django/Sprint 08 Unittest/Task 3.py
@@ -0,0 +1,16 @@
+# Write the function quadratic_equation with arguments a, b ,c that solution to quadratic equation without a complex solution.
+
+# Write unit tests for this function with QuadraticEquationTest class
+
+# Minimum 3 tests: discriminant < 0, discriminant > 0, discriminant = 0
+
+# Заготовка кода:
+# import unittest
+
+
+# def quadratic_equation(a, b, c):
+#     pass
+
+
+# class QuadraticEquationTest(unittest.TestCase):
+#     pass
